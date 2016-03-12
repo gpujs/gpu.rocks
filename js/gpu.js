aa713276b75c56205d3c7da50e772261d65ad3a2
Fazli Sapuan <fazli@sapuan.org>
2016-03-12 14:14:31 +0800

    Update gpu.js

--- js/gpu.js
+++ js/gpu.js
@@ -3792,7 +3792,8 @@ var functionBuilder = (function() {
 			var textureCount = 0;
 			for (textureCount=0; textureCount<paramNames.length; textureCount++) {
 				var paramDim, paramSize, texture;
-				if (Array.isArray(arguments[textureCount])) {
+				var argType = getArgumentType(arguments[textureCount]);
+				if (argType == "Array") {
 					paramDim = getDimensions(arguments[textureCount], true);
 					paramSize = dimToTexSize(gpu, paramDim);
 
@@ -3832,11 +3833,11 @@ var functionBuilder = (function() {
 						gl.uniform2fv(paramSizeLoc, paramSize);
 					}
 					gl.uniform1i(paramLoc, textureCount);
-				} else if (typeof arguments[textureCount] == "number") {
+				} else if (argType == "Number") {
 					var argLoc = gl.getUniformLocation(program, "user_"+paramNames[textureCount]);
 					gl.uniform1f(argLoc, arguments[textureCount]);
-				} else if (arguments[textureCount] instanceof GPUTexture) {
-					paramDim = getDimensions(arguments[textureCount]);
+				} else if (argType == "Texture") {
+					paramDim = getDimensions(arguments[textureCount], true);
 					paramSize = arguments[textureCount].size;
 					texture = arguments[textureCount].texture;
 					textures[textureCount] = texture;
