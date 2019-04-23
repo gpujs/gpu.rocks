f45d0a5d64a7add2f5ca2ef7bc75d59607b544cc
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-23 21:46:16 +0530

    changes

---
+++ src/js/libs.js
@@ -0,0 +1,5 @@
+require('../../node_modules/jquery/dist/jquery');
+require('../../node_modules/jquery.easing/jquery.easing');
+require('../../node_modules/materialize-css/dist/js/materialize');
+require('../../node_modules/github-buttons/dist/buttons');
+require('../../node_modules/highlight.js/lib/highlight');
