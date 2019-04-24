2427e437afbdb5c3cb464e18f79df5742cadadc7
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-24 19:45:56 +0530

    content change

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-materialize'
+
+import threads from '../../img/threads.png'
 import thread from '../../img/thread.png'
 import './Content.scss'
 
 const Content = () => {
  return (
-   <div id="content">
-    <img className="thread" src={thread} alt="thread" />
-    <img className="thread" src={thread} alt="thread" />
-    <img className="thread" src={thread} alt="thread" />
-    <img className="thread" src={thread} alt="thread" />
-    <img className="thread" src={thread} alt="thread" />
-   </div>
+  <Container id="content">
+    <Row>
+      <Col m={12} l={6} className="center" style={{marginBottom: '3rem'}}>
+        <img src={threads} className="threads" alt="threads" />
+        <h6 className="blue-grey-text text-darken-2"><b>Perform massively parallel GPGPU computations using GPU.</b></h6>
+      </Col>
+      <Col m={12} l={6} className="center">
+        <img src={thread} className="thread" alt="thread" />
+        <h6 className="blue-grey-text text-darken-2"><b>Graceful pure JavaScript fallback when GPU is not available. </b></h6>
+      </Col>
+    </Row>
+  </Container>
  )
 }
 
