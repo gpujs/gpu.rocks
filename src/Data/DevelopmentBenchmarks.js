bb34df2ee9010be52cd1fd04f87a268984b8d7e2
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-27 20:57:06 +0530

    add data

---
+++ src/Data/DevelopmentBenchmarks.js
@@ -0,0 +1,15 @@
+import FirefoxData from './gtx-1080/firefox'
+import ChromeData from './gtx-1080/chrome'
+import EdgeData from './gtx-1080/edge'
+
+export const gtx1080 = {
+  firefox: FirefoxData,
+  chrome: ChromeData,
+  edge: EdgeData
+}
+
+const DevelopmentBenchmarks = {
+  gtx1080
+}
+
+export default DevelopmentBenchmarks
