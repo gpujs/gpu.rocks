19c157f4352f57fd7930fa3dd6a94736d4add5a9
Harsh <mjkgoa@gmail.com>
2019-04-28 14:19:51 +0530

    add benchmark component

---
+++ src/Components/Benchmark/Benchmark.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import PageFooter from '../PageFooter/PageFooter'
+
+const Benchmark = () => {
+  return (
+    <div>
+      <Nav />
+      <PageFooter />
+    </div>
+  )
+}
