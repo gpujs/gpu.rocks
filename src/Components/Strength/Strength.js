4d4470c627118f8ccaf9656f1e0d6f05e2ae312d
Harsh <mjkgoa@gmail.com>
2019-06-02 21:28:19 +0530

    nodejs boast

--- src/Components/Strength/Strength.js
+++ src/Components/Strength/Strength.js
@@ -4,6 +4,7 @@ import { animated, useSpring } from 'react-spring'
 
 import threads from '../../img/threads.png'
 import thread from '../../img/thread.png'
+import nodejs from '../../img/nodejs.png'
 
 const Strength = (props) => {
   const config = {
@@ -15,7 +16,7 @@ const Strength = (props) => {
   const {x, opacity} = useSpring({
     config,
     x: props.active ? 0 : 100,
-    opacity: 1,
+    opacity: props.active ? 1 : 0,
     from: {
       x: 100,
       opacity: 0
@@ -23,20 +24,30 @@ const Strength = (props) => {
   })
 
   return (
-    <Row id="strength" style={{overflow: 'hidden'}}>
-      <Col m={12} l={6} className="center" style={{marginBottom: '3rem'}}>
-        <animated.div style={{transform: x.interpolate(x => `translateX(${-x}vw)`), opacity}} >
-          <img src={threads} className="threads" alt="threads" />
-          <h6 className="blue-grey-text text-darken-2"><b>Perform massively parallel GPGPU computations using GPU.</b></h6>
-        </animated.div>
-      </Col>
-      <Col m={12} l={6} className="center" >
-        <animated.div style={{transform: x.interpolate(x => `translateX(${x}vw)`), opacity}} >
-          <img src={thread} className="thread" alt="thread" />
-          <h6 className="blue-grey-text text-darken-2"><b>Graceful pure JavaScript fallback when GPU is not available. </b></h6>
-        </animated.div>
-      </Col>
-    </Row>
+    <div id="strength">
+      <Row style={{overflow: 'hidden'}}>
+        <Col m={12} l={6} className="center" style={{marginBottom: '3rem'}}>
+          <animated.div style={{transform: x.interpolate(x => `translateX(${-x}vw)`), opacity}} >
+            <img src={threads} className="threads" alt="threads" />
+            <h6 className="blue-grey-text text-darken-2"><b>Perform massively parallel GPGPU computations using GPU.</b></h6>
+          </animated.div>
+        </Col>
+        <Col m={12} l={6} className="center" >
+          <animated.div style={{transform: x.interpolate(x => `translateX(${x}vw)`), opacity}} >
+            <img src={thread} className="thread" alt="thread" />
+            <h6 className="blue-grey-text text-darken-2"><b>Graceful pure JavaScript fallback when GPU is not available. </b></h6>
+          </animated.div>
+        </Col>
+      </Row>
+      <Row>
+        <Col s={12} className="center" style={{marginBottom: '3rem'}}>
+          <animated.div style={{opacity}} >
+            <img src={nodejs} className="nodejs" alt="nodejs" />
+            <h5 className="blue-grey-text text-darken-2"><b>Works in <a href="https://nodejs.org">NodeJS</a>!</b></h5>
+          </animated.div>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
