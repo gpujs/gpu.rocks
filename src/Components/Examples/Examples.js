4102ee0855e653ace4b2c2b180ad37858bd4129e
Harsh <mjkgoa@gmail.com>
2019-07-05 23:58:30 +0530

    add some examples

--- src/Components/Examples/Examples.js
+++ src/Components/Examples/Examples.js
@@ -1,55 +1,52 @@
 import React, { Component } from 'react'
 import Heading from '../Heading/Heading'
-import { Card, CardTitle, Row, Col, Container } from 'react-materialize'
+import { Row, Col, Container } from 'react-materialize'
+import M from 'materialize-css'
+
+import mandelbrotSet from '../../img/examples/mandelbrot-set.png'
+import slowFade from '../../img/examples/slow-fade.png'
 
 export class Examples extends Component {
+  componentDidMount() {
+    const elems = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(elems);
+  }
+
   render() {
     return (
       <div>
         <Heading active={true}>Examples</Heading>
         <Container>
           <Row>
-            <Col m={4} s={6}>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={1}>
-                Example 1
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={2}>
-                Example 2
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={3}>
-                Example 3
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={4}>
-                Example 4
-              </Card>
+            <Col s={6}>
+              <div className="card">
+                <div className="card-image">
+                  <img src={slowFade} alt="slow-fade" className="materialboxed" />
+                  <span class="card-title">Slow Fade</span>
+                </div>
+                <div className="card-content">
+                  <p>A simple example wherein colors slowly fade in and fade out.</p>
+                </div>
+                <div className="card-action">
+                  <a href="https://observablehq.com/@robertleeplummerjr/gpu-js-example-slow-fade">Observable JS Notebook</a>
+                </div>
+              </div>
             </Col>
-            <Col m={4} s={6}>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={1}>
-                Example 1
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={2}>
-                Example 2
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={3}>
-                Example 3
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={4}>
-                Example 4
-              </Card>
-            </Col>
-            <Col m={4} s={6}>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={1}>
-                Example 1
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={2}>
-                Example 2
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={3}>
-                Example 3
-              </Card>
-              <Card header={<CardTitle />} actions={[<a href="https://github.com/something">Link</a>]} key={4}>
-                Example 4
-              </Card>
+
+            <Col s={6}>
+              <div className="card">
+                <div className="card-image">
+                  <img src={mandelbrotSet} alt="mandelbrot-set" className="materialboxed" />
+                  <span class="card-title">Mandelbrot Set</span>
+                </div>
+                <div className="card-content">
+                  <p>A plot of a particular set of complex numbers called the Mandelbrot set.</p>
+                </div>
+                <div className="card-action">
+                  <a href="https://observablehq.com/@robertleeplummerjr/gpu-js-example-slow-fade">Observable JS Notebook</a>
+                  <a href="https://en.wikipedia.org/wiki/Mandelbrot_set">Wikipedia</a>
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
