303783cdeb17d2763e4e53d2498befc668051573
Harsh <mjkgoa@gmail.com>
2019-08-26 00:03:41 +0530

    change navbar brand to NavLink

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import HeaderLinks from '../HeaderLinks/HeaderLinks'
 import { Navbar } from 'react-materialize'
+import { NavLink } from 'react-router-dom'
 
 import './Nav.scss'
 
 const Nav = () => {
   return (
     <div id="nav">
-      <Navbar brand={<a href="/#" style={{marginLeft: '1rem'}}>GPU.js</a>} alignLinks="right" >
+      <Navbar brand={<NavLink to="/" style={{marginLeft: '1rem'}}>GPU.js</NavLink>} alignLinks="right" >
         <HeaderLinks />
       </Navbar>
     </div>
