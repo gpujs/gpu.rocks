c83f50a195851993d91efa64b5b55ec503aedee9
Robert Plummer <robertleeplummerjr@gmail.com>
2019-09-15 08:30:45 -0400

    fix: Return null for chart for tie being, till we get it working

--- src/Components/Util/Graph/Graph.js
+++ src/Components/Util/Graph/Graph.js
@@ -6,34 +6,35 @@ import legend from 'chartist-plugin-legend'
 import 'chartist/dist/chartist.css'
 
 const Graph = () => {
-  const data = {
-    labels: ['One','Two','Three','Four','Five'],
-    series: [
-      [12, 9, 7, 8, 5],
-      [2, 1, 3.5, 7, 3],
-      [1, 3, 4, 5, 6]]
-  };
-
-  const options = {
-    lineSmooth: Chartist.Interpolation.none(),
-    low: 0,
-    high: 12,
-    width: '800px',
-    height: '200px',
-    plugins: [
-      legend({
-        legendNames: ['Blue pill', 'Red pill', 'Purple pill'],
-      })
-    ]
-  }
-  return (
-    <ChartistGraph
-      className="ct-chart"
-      type="Line"
-      options={options}
-      data={data}
-    />
-  )
+  return null
+  // const data = {
+  //   labels: ['One','Two','Three','Four','Five'],
+  //   series: [
+  //     [12, 9, 7, 8, 5],
+  //     [2, 1, 3.5, 7, 3],
+  //     [1, 3, 4, 5, 6]]
+  // };
+  //
+  // const options = {
+  //   lineSmooth: Chartist.Interpolation.none(),
+  //   low: 0,
+  //   high: 12,
+  //   width: '800px',
+  //   height: '200px',
+  //   plugins: [
+  //     legend({
+  //       legendNames: ['Blue pill', 'Red pill', 'Purple pill'],
+  //     })
+  //   ]
+  // }
+  // return (
+  //   <ChartistGraph
+  //     className="ct-chart"
+  //     type="Line"
+  //     options={options}
+  //     data={data}
+  //   />
+  // )
 }
 
 export default Graph
