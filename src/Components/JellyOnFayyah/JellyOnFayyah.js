1ff0871caefa0eba578700e8ef0f4d6bb25b78bb
Robert Plummer <robertleeplummerjr@gmail.com>
2019-10-18 23:08:01 -0400

    feat: Add some fayyahs

--- src/Components/JellyOnFayyah/JellyOnFayyah.js
+++ src/Components/JellyOnFayyah/JellyOnFayyah.js
@@ -1,46 +1,84 @@
-import { animated, useSpring } from 'react-spring'
-
-import React from 'react'
-import fayyah from './fayyah.webp'
+import React, { Component } from 'react'
 import logo from '../../img/jelly.png'
-import mask from './jelly-transparent.png'
+import fire1 from '../../img/fire1.gif'
+import fire2 from '../../img/fire2.gif'
 
-export default function JellyOnFayyah() {
-  const {y} = useSpring({
-    config: {
-      mass: 3,
-      tension: 100,
-      friction: 20
-    },
-    y: 0,
-    from: {
-      y: 100
+const alt = 'GPU.js logo';
+function getImage(url, height, maxWidth) {
+  return (<img src={url} alt={alt} style={{
+    height,
+    maxWidth,
+  }} />);
+}
+
+export default class JellyOnFayyah extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logoIndex: -1
+    };
+    this.logos = [getImage(fire1, '370px', '370px'), getImage(fire2, '350px', '350px')];
+    this.activeTimeout = null;
+    this.img = null;
+    this.setImg = this.setImg.bind(this);
+    this.ignite = this.ignite.bind(this);
+    this.setIgnition = this.setIgnition.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  ignite(logoIndex) {
+    if (this.state.logoIndex > -1) return;
+    this.setState({logoIndex});
+    this.cool();
+  }
+
+  cool() {
+    if (this.activeTimeout || this.state.logoIndex === -1) return;
+    this.activeTimeout = setTimeout(() => {
+      this.setState({
+        logoIndex: -1
+      });
+      this.activeTimeout = null;
+    }, Math.random() * 3000);
+  }
+
+  setIgnition() {
+    setInterval(this.tick, 200);
+  }
+
+  tick() {
+    if (this.activeTimeout) return;
+    if (Math.random() > 0.965) {
+      const logoIndex = Math.floor(Math.random() * this.logos.length);
+      this.ignite(logoIndex);
     }
-  })
-
-  const ignite = () => {
-    const mainLogo = document.querySelector('#main-logo'),
-      fayyah = document.querySelector('#fayyah')
-
-    mainLogo.style.display = mainLogo.style.display == 'none' ? 'initial': 'none';
-    fayyah.style.display = fayyah.style.display == 'initial' ? 'none' : 'initial';
   }
 
-  const setIgnition = () => {
-    setInterval(() => {
-      if (Math.random() > Math.random() * 10) ignite();
-    }, 200)
+  setImg(img) {
+    if (!img) return;
+    this.img = img;
   }
-  
-  return (
-    <div>
-      <animated.img src={logo} style={{transform: y.interpolate(y => `translateY(${y}%)`)}} alt="logo" id="main-logo" />
-      <img src={mask} style={{
-        backgroundImage: `url(${fayyah})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        display: 'none'
-      }} alt="fayyah" id="fayyah" onLoad={setIgnition} />
-    </div>
-  )
+
+  render() {
+    return (
+      <div style={{
+        width: '100%',
+        height: '300px',
+      }}>
+        {
+          this.state.logoIndex === -1
+            ? (
+                <img
+                  style={{ maxWidth: '280px', paddingTop: '40px', paddingBottom: '-40px' }}
+                  ref={this.setImg}
+                  src={logo}
+                  alt={alt}
+                  onLoad={this.setIgnition}
+                />
+              )
+            : this.logos[this.state.logoIndex]
+        }
+      </div>
+    )
+  }
 }
