b339998da5e14cc55f6410ffced40e2b4b5ca6f3
Mr. X <mjkgoa@gmail.com>
2019-10-20 23:08:13 +0530

    add github star and fork buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,33 @@
-import './Header.scss'
-
 import JellyOnFayyah from '../JellyOnFayyah/JellyOnFayyah'
 import React from 'react'
 import Row from 'react-materialize/lib/Row'
-import logo from '../../img/jelly.png'
+import GitHubButton from 'react-github-btn'
+
+import './Header.scss'
 
 const Header = () => {
   const titleComponents = [
-    <h1 className="name">GPU.js</h1>,
-    <h5 className="desc"><b>GPU accelerated JavaScript</b></h5>
+    <h1 className="name" key={1}>GPU.js</h1>,
+    <h5 className="desc" key={2}><b>GPU accelerated JavaScript</b></h5>,
+    <GitHubButton 
+      href="https://github.com/gpujs/gpu.js"
+      data-color-scheme="no-preference: dark; light: dark; dark: dark;" 
+      data-size="large" 
+      data-icon="octicon-star"
+      data-show-count="true" 
+      style={{marginRight: '3rem'}}
+      aria-label="Star gpujs/gpu.js on GitHub"
+      key={3}
+    >Star</GitHubButton>,
+    <GitHubButton
+      href="https://github.com/gpujs/gpu.js/fork"
+      data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+      data-icon="octicon-repo-forked"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Fork gpujs/gpu.js on GitHub"
+      key={4}
+    >Fork</GitHubButton>
   ]
 
   return (
@@ -17,6 +36,7 @@ const Header = () => {
         <Row className="center responsive-img">
           <JellyOnFayyah />
         </Row>
+        
         <Row className="center">
           {titleComponents}
         </Row>
