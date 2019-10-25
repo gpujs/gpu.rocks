905fe5a461d6eb746a9e2ef31e91fdfecdecb680
Mr. X <mjkgoa@gmail.com>
2019-10-25 23:35:58 +0530

    change the copy code btn

--- src/Components/Util/Code/Code.js
+++ src/Components/Util/Code/Code.js
@@ -28,12 +28,13 @@ const Code = (props) => {
   }
 
   return (
-    <div id="code">
+    <div className="code">
       <Tooltip content="Copy" placement="left" className="right" radius={10} padding={8} fadeDuration={400} >
         <Button floating waves="light" className="grey darken-3" style={{marginTop: '1rem', marginRight: '1rem'}} onClick={() => copy(props.code, () => M.toast({html: 'Code Copied', classes: 'rounded'}))}>
           <MaterialIcon icon="content_copy" size="small" color="white" />
         </Button>
       </Tooltip>
+
       <SyntaxHighlighter language={props.language || 'javascript'} style={monokai}>{props.code}</SyntaxHighlighter>
     </div>
   )
