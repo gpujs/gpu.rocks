4978c159f0e029bc05d4a1616bf26d7030b102b5
samu698 <50919097+samu698@users.noreply.github.com>
2019-12-15 19:07:18 +0100

    Replaced var with let
    Replaced var declaration in kernel because they are deprecated.

--- src/Components/Example/CodeExamples.js
+++ src/Components/Example/CodeExamples.js
@@ -19,8 +19,8 @@ export const createKernel =
 `
   const gpu = new GPU();
   const multiplyMatrix = gpu.createKernel(function(a, b) {
-    var sum = 0;
-    for (var i = 0; i < 512; i++) {
+    let sum = 0;
+    for (let i = 0; i < 512; i++) {
       sum += a[this.thread.y][i] * b[i][this.thread.x];
     }
     return sum;
