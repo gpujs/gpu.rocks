1c417b4f284eb100175574e11d5d688b14050001
Mr. X <mjkgoa@gmail.com>
2020-01-26 20:33:29 +0530

    footer change

--- src/Components/PageFooter/PageFooter.js
+++ src/Components/PageFooter/PageFooter.js
@@ -22,7 +22,7 @@ const PageFooter = () => {
           </li>
         </ul>
         }
-      moreLinks={<div className="center"><span>&copy; {new Date().getFullYear()} <b>GPU.js</b> Org</span> | Images(except logo) by <a href="https://pixabay.com/users/CopyrightFreePictures-203/" className="white-text">CopyrightFreePictures</a> from <a href="https://pixabay.com/" className="white-text">Pixabay</a></div>}
+      moreLinks={<div className="center"><span>&copy; {new Date().getFullYear()} <b>GPU.js</b> Org</span> | Created by the GPU.js Community | Images(except logo) by <a href="https://pixabay.com/users/CopyrightFreePictures-203/" className="white-text">CopyrightFreePictures</a> from <a href="https://pixabay.com/" className="white-text">Pixabay</a></div>}
       className="sticky"
     >
       <h6>
