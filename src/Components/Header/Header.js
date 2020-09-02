ed2b21145273b893baaa123e402cbc1c5b2b2667
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-02 16:07:38 +0530

    fix: add start and fork buttons again

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import JellyOnFayyah from '../JellyOnFayyah/JellyOnFayyah'
 import React from 'react'
 import Row from 'react-materialize/lib/Row'
-// import GitHubButton from 'react-github-btn'
+import GitHubButton from 'react-github-btn'
 
 import './Header.scss'
 
@@ -9,25 +9,24 @@ const Header = () => {
   const titleComponents = [
     <h1 className="name" key={1}>GPU.js</h1>,
     <h5 className="desc" key={2}><b>GPU accelerated JavaScript</b></h5>,
-    // <GitHubButton
-    //   href="https://github.com/gpujs/gpu.js"
-    //   data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-    //   data-size="large"
-    //   data-icon="octicon-star"
-    //   data-show-count="true"
-    //   style={{marginRight: '3rem'}}
-    //   aria-label="Star gpujs/gpu.js on GitHub"
-    //   key={3}
-    // >Star</GitHubButton>,
-    // <GitHubButton
-    //   href="https://github.com/gpujs/gpu.js/fork"
-    //   data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-    //   data-icon="octicon-repo-forked"
-    //   data-size="large"
-    //   data-show-count="true"
-    //   aria-label="Fork gpujs/gpu.js on GitHub"
-    //   key={4}
-    // >Fork</GitHubButton>
+    <span style={{marginRight: '2rem'}}><GitHubButton
+      href="https://github.com/gpujs/gpu.js"
+      data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+      data-size="large"
+      data-icon="octicon-star"
+      data-show-count="true"
+      aria-label="Star gpujs/gpu.js on GitHub"
+      key={3}
+    >Star</GitHubButton></span>,
+    <GitHubButton
+      href="https://github.com/gpujs/gpu.js/fork"
+      data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+      data-icon="octicon-repo-forked"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Fork gpujs/gpu.js on GitHub"
+      key={4}
+    >Fork</GitHubButton>
   ]
 
   return (
