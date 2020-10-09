ae5861ac30b4d0544fca031d2dd6e92b4b2b4039
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-09 17:57:01 +0530

    fix: render page only if https, and enable service worker

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 if (window.location.protocol === 'http:' && window.location.hostname === 'gpu.rocks') window.location.protocol = 'https://';
+else {
+  ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.register();
+}
