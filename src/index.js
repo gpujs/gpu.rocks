2513b67e96ad32e2efc109249518c7a7fb2505eb
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-09 17:46:03 +0530

    fix: enforce https

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+if (window.location.protocol === 'http:' && window.location.origin === 'gpu.rocks') window.location.protocol = 'https://';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
